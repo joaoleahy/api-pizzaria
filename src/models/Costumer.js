7f2402e34104f0346fe4801e979f46a8ffc2f91b
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-10-02 18:04:42 -0300

    feat(models/Costumer): cria nova model para clientes

---
+++ src/models/Costumer.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const costumersSchema = mongoose.Schema({
+    id: { type: String, unique: true },
+    name: { type: String, required: true },
+    phoneNumber: { type: String, unique: true, required: true },
+    address: { type: Array, required: true }
+});
+
+const Clientes = mongoose.model("costumers", costumersSchema);
+
+module.exports = Clientes;

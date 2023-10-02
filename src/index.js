7246658a11490258b889c582799e6f3a975dc58b
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-10-02 18:54:36 -0300

    feat(index): adiciona as novas rotas no app

--- src/index.js
+++ src/index.js
@@ -13,11 +13,17 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerConfig));
 
 const auth =  require("./router/AuthRouter");
 const user =  require("./router/UserRouter");
+const costumer =  require("./router/CostumerRouter");
+const product =  require("./router/ProductRouter");
+const order =  require("./router/OrderRouter");
 
 app.use(express.json());
 
 app.use("/auth", auth);
 app.use("/users", user);
+app.use('/costumers', costumer);
+app.use('/products', product);
+app.use('/orders', order);
 
 app.listen(port, () => {
     console.log(`Servicor rodando em http://localhost:${port}`)

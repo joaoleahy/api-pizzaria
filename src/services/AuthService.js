bd834475acf57e79862d776210348d0cc5492766
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-10-02 17:40:06 -0300

    feat(services/AuthService): implementa service para autenticação e token JWT

---
+++ src/services/AuthService.js
@@ -0,0 +1,21 @@
+const User = require("../model/usuario.model");
+const jwt = require("jsonwebtoken");
+
+const secret = "f94if94i9sd1010";
+
+const loginService = (email) => User.findOne({ email });
+
+const gerarTokenJWT = (user) =>
+    jwt.sign({ user }, secret, {
+        expiresIn: 86400
+    });
+
+const validarTokenJWT = (token) =>
+    jwt.verify(token, secret);
+
+
+module.exports = {
+    loginService,
+    gerarTokenJWT,
+    validarTokenJWT
+};

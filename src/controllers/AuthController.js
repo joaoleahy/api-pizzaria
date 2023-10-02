2bf3c8c5f3501a9342f4d3caa7fd49c16069f6bb
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-10-02 17:36:46 -0300

    feat(controllers/AuthController): implementa controller de autenticação do usuário no sistema

---
+++ src/controllers/AuthController.js
@@ -0,0 +1,25 @@
+const service = require("../service/auth.service");
+
+const auth = (req, res) => {
+    const { email, senha } = req.body;
+
+    service.loginService(email)
+        .then((usuario) => {
+            if(!usuario) {
+                return res.status(404).send({ message: "Usuario não encontrado! Tente novamente!"});
+            }
+
+            if(usuario.senha != senha) {
+                return res.status(404).send({ message: "Usuario não encontrado! Tente novamente!"});
+            }
+
+            const token = service.gerarTokenJWT(usuario);
+
+            return res.status(200).send({ auth: true, token });
+        })
+        .catch((err) => {
+            return res.status(500).send({ message: err.message });
+        });
+}
+
+module.exports = { auth }

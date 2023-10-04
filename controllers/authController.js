355e8caca05be54e89cb0e11188ddee227d7ae19
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-10-04 20:14:16 -0300

    refactor(authController): remove comentários desnecessários

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,17 @@
 import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv'; // Importe a biblioteca dotenv para carregar variáveis de ambiente
+import dotenv from 'dotenv';
 
-dotenv.config(); // Carregue as variáveis de ambiente do arquivo .env
+dotenv.config(); 
 
-// Função de login
 export const login = (req, res) => {
   const { username, password } = req.body;
 
-  // Verifique as credenciais (substitua isso com sua lógica de autenticação real)
   if (username === 'admin' && password === '4queijos') {
-    // Crie um token JWT usando a chave secreta do .env
-    const token = jwt.sign({ username }, process.env.SECRET_KEY, { expiresIn: 86400 }); // O token expira em 1 hora
+    // Cria um token JWT usando a chave secreta do .env
+    const token = jwt.sign({ username }, process.env.SECRET_KEY, { expiresIn: 86400 });
 
-    // Envie o token JWT como resposta
     res.json({ token });
   } else {
-    // Se as credenciais estiverem incorretas, envie uma resposta de erro
     res.status(401).json({ message: 'Credenciais inválidas' });
   }
 };
